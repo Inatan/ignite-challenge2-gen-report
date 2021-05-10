caa91aeb75a3ced990b9eb573c36daa472f41aaa
Inatan <inatan_hertzog@hotmail.com>
2021-05-10 15:19:24 -0300

    Finish Challenge with test

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,18 +1,88 @@
 defmodule GenReport do
-  @moduledoc """
-  Documentation for `GenReport`.
-  """
+  alias GenReport.Parser
 
-  @doc """
-  Hello world.
+  @available_names [
+    "Cleiton",
+    "Daniele",
+    "Danilo",
+    "Diego",
+    "Giuliano",
+    "Jakeliny",
+    "Joseph",
+    "Mayk",
+    "Rafael",
+    "Vinicius"
+  ]
 
-  ## Examples
+  @available_months %{
+    1 => "janeiro",
+    2 => "fevereiro",
+    3 => "março",
+    4 => "abril",
+    5 => "maio",
+    6 => "junho",
+    7 => "julho",
+    8 => "agosto",
+    9 => "setembro",
+    10 => "outubro",
+    11 => "novembro",
+    12 => "dezembro"
+  }
 
-      iex> GenReport.hello()
-      :world
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn list, report -> sum_value(list, report) end)
+  end
 
-  """
-  def hello do
-    :world
+  defp sum_value([nome, quantidadeHoras, _dia, mes, ano], %{
+         "all_hours" => all_hours,
+         "hours_per_month" => hours_per_month,
+         "hours_per_year" => hours_per_year
+       }) do
+    all_hours = Map.put(all_hours, nome, all_hours[nome] + quantidadeHoras)
+
+    hours_per_month =
+      Map.put(
+        hours_per_month,
+        nome,
+        Map.put(
+          hours_per_month[nome],
+          @available_months[mes],
+          hours_per_month[nome][@available_months[mes]] + quantidadeHoras
+        )
+      )
+
+    hours_per_year =
+      Map.put(
+        hours_per_year,
+        nome,
+        Map.put(
+          hours_per_year[nome],
+          ano,
+          hours_per_year[nome][ano] + quantidadeHoras
+        )
+      )
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp report_acc do
+    all_hours = Enum.into(@available_names, %{}, &{&1, 0})
+    all_years_per_user = Enum.into(2016..2020, %{}, &{&1, 0})
+    hours_per_year = Enum.into(@available_names, %{}, &{&1, all_years_per_user})
+
+    all_month_per_user = Enum.into(1..12, %{}, &{@available_months[&1], 0})
+    hours_per_month = Enum.into(@available_names, %{}, &{&1, all_month_per_user})
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp build_report(all_hours, hours_per_month, hours_per_year) do
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
   end
 end
--- test/gen_report_test.exs
+++ test/gen_report_test.exs
@@ -1,8 +1,182 @@
 defmodule GenReportTest do
   use ExUnit.Case
-  doctest GenReport
 
-  test "greets the world" do
-    assert GenReport.hello() == :world
+  describe "build/1" do
+    test "builds the report" do
+      filename = "gen_report_test.csv"
+
+      response = GenReport.build(filename)
+
+      expected_response = %{
+        "all_hours" => %{
+          "Cleiton" => 1,
+          "Daniele" => 12,
+          "Danilo" => 0,
+          "Diego" => 0,
+          "Giuliano" => 9,
+          "Jakeliny" => 14,
+          "Joseph" => 3,
+          "Mayk" => 5,
+          "Rafael" => 0,
+          "Vinicius" => 0
+        },
+        "hours_per_month" => %{
+          "Cleiton" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 1,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Daniele" => %{
+            "abril" => 7,
+            "agosto" => 0,
+            "dezembro" => 5,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Danilo" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Diego" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Giuliano" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 9,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Jakeliny" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 8,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 6,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Joseph" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 3,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Mayk" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 5,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Rafael" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          },
+          "Vinicius" => %{
+            "abril" => 0,
+            "agosto" => 0,
+            "dezembro" => 0,
+            "fevereiro" => 0,
+            "janeiro" => 0,
+            "julho" => 0,
+            "junho" => 0,
+            "maio" => 0,
+            "março" => 0,
+            "novembro" => 0,
+            "outubro" => 0,
+            "setembro" => 0
+          }
+        },
+        "hours_per_year" => %{
+          "Cleiton" => %{2016 => 0, 2017 => 0, 2018 => 0, 2019 => 0, 2020 => 1},
+          "Daniele" => %{2016 => 5, 2017 => 0, 2018 => 7, 2019 => 0, 2020 => 0},
+          "Danilo" => %{2016 => 0, 2017 => 0, 2018 => 0, 2019 => 0, 2020 => 0},
+          "Diego" => %{2016 => 0, 2017 => 0, 2018 => 0, 2019 => 0, 2020 => 0},
+          "Giuliano" => %{2016 => 0, 2017 => 3, 2018 => 0, 2019 => 6, 2020 => 0},
+          "Jakeliny" => %{2016 => 0, 2017 => 8, 2018 => 0, 2019 => 6, 2020 => 0},
+          "Joseph" => %{2016 => 0, 2017 => 3, 2018 => 0, 2019 => 0, 2020 => 0},
+          "Mayk" => %{2016 => 0, 2017 => 1, 2018 => 0, 2019 => 4, 2020 => 0},
+          "Rafael" => %{2016 => 0, 2017 => 0, 2018 => 0, 2019 => 0, 2020 => 0},
+          "Vinicius" => %{2016 => 0, 2017 => 0, 2018 => 0, 2019 => 0, 2020 => 0}
+        }
+      }
+
+      assert response == expected_response
+    end
   end
 end
